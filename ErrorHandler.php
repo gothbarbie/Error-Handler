3b3bb38443d23154ab4f39ceea4c277c65eba442
Hanna <gothbarbie84@gmail.com>
2016-01-29 12:41:09 +0100

    First commit

---
+++ ErrorHandler.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *  Handlers Errors
+ */
+class ErrorHandler
+{
+    protected $errors = [];
+
+    public function addError($error, $key = null)
+    {
+        if ($key)
+        {
+            $this->errors[$key][] = $error;
+        }
+        else
+        {
+            $this->errors[] = $error;
+        }
+    }
+
+    public function all($key = null)
+    {
+        return isset($this->errors[$key]) ? $this->errors[$key] : $this->errors;
+    }
+
+    public function hasErrors()
+    {
+        return count($this->all()) ? true : false;
+    }
+
+    // @return string
+    public function first($key)
+    {
+        return isset($this->all()[$key][0]) ? $this->all()[$key][0] : '';
+    }
+}
